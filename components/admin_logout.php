847083037b6531d87e840a584a57b2fdfa7db212
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-04 12:15:12 -0400

    Update admin_logout.php

--- components/admin_logout.php
+++ components/admin_logout.php
@@ -1,29 +1,15 @@
 <?php
 
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Educação</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="images/favicon.ico"/>
-</head>
-<body>
+   include 'connect.php';
 
-<?php
-	echo "Olá Mundo!";
-?>
-	
-	<script src="js/script.js"></script>
-</body>
-</html>
+   setcookie('tutor_id', '', time() - 1, '/');
 
+   header('location:../admin/login.php');
+
+?>
 <!--
     Autor: Daniel Oliveira
     Email: danieloliveira.webmaster@gmail.com
     Manaus/Amazonas
-    24/10/2022
+    04/02/2023
 -->
