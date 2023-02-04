10074b3da67d4221b54b1c5f313111b30121bd9a
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-04 13:07:11 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,23 +1,84 @@
 <?php
 
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+if(isset($_POST['submit'])){
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ? LIMIT 1");
+   $select_user->execute([$email, $pass]);
+   $row = $select_user->fetch(PDO::FETCH_ASSOC);
+   
+   if($select_user->rowCount() > 0){
+     setcookie('user_id', $row['id'], time() + 60*60*24*30, '/');
+     header('location:home.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Educação</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="images/favicon.ico"/>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
 
-<?php
-	echo "Olá Mundo!";
-?>
-	
-	<script src="js/script.js"></script>
+<?php include 'components/user_header.php'; ?>
+
+<section class="form-container">
+
+   <form action="" method="post" enctype="multipart/form-data" class="login">
+      <h3>welcome back!</h3>
+      <p>your email <span>*</span></p>
+      <input type="email" name="email" placeholder="enter your email" maxlength="50" required class="box">
+      <p>your password <span>*</span></p>
+      <input type="password" name="pass" placeholder="enter your password" maxlength="20" required class="box">
+      <p class="link">don't have an account? <a href="register.php">register now</a></p>
+      <input type="submit" name="submit" value="login now" class="btn">
+   </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
 </body>
 </html>
 
@@ -25,5 +86,5 @@
     Autor: Daniel Oliveira
     Email: danieloliveira.webmaster@gmail.com
     Manaus/Amazonas
-    24/10/2022
+    04/02/2023
 -->
