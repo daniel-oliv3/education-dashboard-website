b2c8168ba9dda6d5aaf20a488aaad64fd85e6fea
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-04 12:26:49 -0400

    Update login.php

--- admin/login.php
+++ admin/login.php
@@ -1,23 +1,113 @@
 <?php
 
+include '../components/connect.php';
+
+if(isset($_POST['submit'])){
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_tutor = $conn->prepare("SELECT * FROM `tutors` WHERE email = ? AND password = ? LIMIT 1");
+   $select_tutor->execute([$email, $pass]);
+   $row = $select_tutor->fetch(PDO::FETCH_ASSOC);
+   
+   if($select_tutor->rowCount() > 0){
+     setcookie('tutor_id', $row['id'], time() + 60*60*24*30, '/');
+     header('location:dashboard.php');
+   }else{
+      $message[] = 'incorrect email or password!';
+   }
+
+}
+
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Educação</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="images/favicon.ico"/>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
 </head>
-<body>
+<body style="padding-left: 0;">
 
 <?php
-	echo "Olá Mundo!";
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message form">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
 ?>
-	
-	<script src="js/script.js"></script>
+
+<!-- register section starts  -->
+
+<section class="form-container">
+
+   <form action="" method="post" enctype="multipart/form-data" class="login">
+      <h3>welcome back!</h3>
+      <p>your email <span>*</span></p>
+      <input type="email" name="email" placeholder="enter your email" maxlength="20" required class="box">
+      <p>your password <span>*</span></p>
+      <input type="password" name="pass" placeholder="enter your password" maxlength="20" required class="box">
+      <p class="link">don't have an account? <a href="register.php">register new</a></p>
+      <input type="submit" name="submit" value="login now" class="btn">
+   </form>
+
+</section>
+
+<!-- registe section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+
+let darkMode = localStorage.getItem('dark-mode');
+let body = document.body;
+
+const enabelDarkMode = () =>{
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enabelDarkMode();
+}else{
+   disableDarkMode();
+}
+
+</script>
+   
 </body>
 </html>
 
@@ -25,5 +115,5 @@
     Autor: Daniel Oliveira
     Email: danieloliveira.webmaster@gmail.com
     Manaus/Amazonas
-    24/10/2022
+    04/02/2023
 -->
