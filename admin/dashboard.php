c8d5ada276ba7f6ffda4ebeab87b91d9402932f9
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-04 12:27:37 -0400

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -26,15 +26,21 @@ $select_comments->execute([$tutor_id]);
 $total_comments = $select_comments->rowCount();
 
 ?>
+
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Educação</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="images/favicon.ico"/>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="../css/admin_style.css">
+
 </head>
 <body>
 
@@ -92,9 +98,21 @@ $total_comments = $select_comments->rowCount();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
 <?php include '../components/footer.php'; ?>
 
 <script src="../js/admin_script.js"></script>
+
 </body>
 </html>
 
@@ -102,5 +120,5 @@ $total_comments = $select_comments->rowCount();
     Autor: Daniel Oliveira
     Email: danieloliveira.webmaster@gmail.com
     Manaus/Amazonas
-    24/10/2022
+    04/02/2023
 -->
