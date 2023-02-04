bf17e38f7a4f18f8a223973ffeaa7607f403eda1
Daniel Oliveira <danielsapup3@gmail.com>
2023-02-04 12:13:40 -0400

    Update user_header.php

--- components/user_header.php
+++ components/user_header.php
@@ -1,29 +1,116 @@
 <?php
-
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Educação</title>
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="shortcut icon" href="images/favicon.ico"/>
-</head>
-<body>
 
-<?php
-	echo "Olá Mundo!";
-?>
-	
-	<script src="js/script.js"></script>
-</body>
-</html>
+<header class="header">
+
+   <section class="flex">
+
+      <a href="home.php" class="logo">Educa.</a>
+
+      <form action="search_course.php" method="post" class="search-form">
+         <input type="text" name="search_course" placeholder="search courses..." required maxlength="100">
+         <button type="submit" class="fas fa-search" name="search_course_btn"></button>
+      </form>
+
+      <div class="icons">
+         <div id="menu-btn" class="fas fa-bars"></div>
+         <div id="search-btn" class="fas fa-search"></div>
+         <div id="user-btn" class="fas fa-user"></div>
+         <div id="toggle-btn" class="fas fa-sun"></div>
+      </div>
+
+      <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3><?= $fetch_profile['name']; ?></h3>
+         <span>student</span>
+         <a href="profile.php" class="btn">view profile</a>
+         <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <a href="components/user_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn">logout</a>
+         <?php
+            }else{
+         ?>
+         <h3>please login or register</h3>
+          <div class="flex-btn">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <?php
+            }
+         ?>
+      </div>
+
+   </section>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- side bar section starts  -->
+
+<div class="side-bar">
+
+   <div class="close-side-bar">
+      <i class="fas fa-times"></i>
+   </div>
+
+   <div class="profile">
+         <?php
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+            $select_profile->execute([$user_id]);
+            if($select_profile->rowCount() > 0){
+            $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+         ?>
+         <img src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+         <h3><?= $fetch_profile['name']; ?></h3>
+         <span>student</span>
+         <a href="profile.php" class="btn">view profile</a>
+         <?php
+            }else{
+         ?>
+         <h3>please login or register</h3>
+          <div class="flex-btn" style="padding-top: .5rem;">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+         <?php
+            }
+         ?>
+      </div>
+
+   <nav class="navbar">
+      <a href="home.php"><i class="fas fa-home"></i><span>home</span></a>
+      <a href="about.php"><i class="fas fa-question"></i><span>about us</span></a>
+      <a href="courses.php"><i class="fas fa-graduation-cap"></i><span>courses</span></a>
+      <a href="teachers.php"><i class="fas fa-chalkboard-user"></i><span>teachers</span></a>
+      <a href="contact.php"><i class="fas fa-headset"></i><span>contact us</span></a>
+   </nav>
+
+</div>
+
+<!-- side bar section ends -->
 
 <!--
     Autor: Daniel Oliveira
     Email: danieloliveira.webmaster@gmail.com
     Manaus/Amazonas
-    24/10/2022
+    04/02/2023
 -->
